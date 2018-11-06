a25d2a1d325b8f4c3c2a8bd321c4afebba3f8b60
PsyFabriQ <psyfabriqdev@gmail.com>
2018-11-07 00:45:48 +0300

    Commit working project )

--- side-borderline/app-pfq-api/src/main/java/ru/psyfabriq/AppServerApiApplication.java
+++ side-borderline/app-pfq-api/src/main/java/ru/psyfabriq/AppServerApiApplication.java
@@ -3,9 +3,11 @@ package ru.psyfabriq;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableResourceServer
 public class AppServerApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(AppServerApiApplication.class, args);
