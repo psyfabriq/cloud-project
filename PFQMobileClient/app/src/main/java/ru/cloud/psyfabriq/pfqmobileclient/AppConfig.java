6e71c5c0a34d9602976187f0af73c959cfb7cf9c
PsyFabriQ <psyfabriqdev@gmail.com>
2019-01-18 03:10:34 +0300

    Start to create Android Client App

---
+++ PFQMobileClient/app/src/main/java/ru/cloud/psyfabriq/pfqmobileclient/AppConfig.java
@@ -0,0 +1,10 @@
+package ru.cloud.psyfabriq.pfqmobileclient;
+
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://192.168.0.102/android_login_api/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://192.168.0.102/android_login_api/register.php";
+
+}
---
+++ PFQMobileClient/app/src/main/java/ru/cloud/psyfabriq/pfqmobileclient/MainActivity.java
@@ -0,0 +1,59 @@
+package ru.cloud.psyfabriq.pfqmobileclient;
+
+
+import java.util.HashMap;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    private TextView txtName;
+    private TextView txtEmail;
+    private Button btnLogout;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txtName = (TextView) findViewById(R.id.name);
+        txtEmail = (TextView) findViewById(R.id.email);
+        btnLogout = (Button) findViewById(R.id.btnLogout);
+
+
+
+
+
+        // Displaying the user details on the screen
+        txtName.setText("dgdfg");
+        txtEmail.setText("kkkk");
+
+        // Logout button click event
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                logoutUser();
+            }
+        });
+    }
+
+    /**
+     * Logging out the user. Will set isLoggedIn flag to false in shared
+     * preferences Clears the user data from sqlite users table
+     * */
+    private void logoutUser() {
+
+        // Launching the login activity
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
